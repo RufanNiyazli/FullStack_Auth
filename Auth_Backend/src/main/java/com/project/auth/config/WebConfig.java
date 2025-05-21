6919bb33174e3bf12a772abcfdc3f5053fed7bef
RufanNiyazli <developerx73@gmail.com>
2025-05-21 22:57:31 +0400

    frontedn bitdi

---
+++ Auth_Backend/src/main/java/com/project/auth/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.project.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Tüm endpointlere izin ver
+                        .allowedOrigins("http://localhost:5174") // React portu
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
